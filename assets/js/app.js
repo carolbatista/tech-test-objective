b606598d933b75277dc42cee9343dda6b046d9c2
carolbatista <carolbatista2@gmail.com>
2021-08-14 16:48:25 -0300

    Search and Pagination

---
+++ assets/js/app.js
@@ -0,0 +1,86 @@
+const publicKey = '5b600b15470a84298961f4f9d17d36b0';
+const hash = '34ddb2ca0be74ea053c0941d64e727ee';
+const timestamp = '1628959565';
+
+const paginationDiv = document.querySelector(".pagination");
+const divResult = document.querySelector("#js-api-marvel-result");
+const prevButton = document.querySelector('#button-prev');
+const nextButton = document.querySelector('#button-next');
+const startButton = document.querySelector('#button-start');
+const endButton = document.querySelector('#button-end');
+const clickPageNumber = document.querySelectorAll('.clickPageNumber');
+
+let current_page = 1;
+let records_per_page = 4;
+let result = [];
+let resultSearch = [];
+
+const options = {
+    method: 'GET',
+    headers: new Headers({
+        'Content-Type': 'application/json'
+    })
+};
+
+function returnItems( items ){
+    let listItems = '';
+
+    for ( i = 0; i <= 2; i++ ){
+        if ( items[i] ){
+            listItems += '<p>' + items[i].name + '</p>';
+        }
+    }
+
+    return listItems;
+}
+
+function thumbnail( data ){
+    return data.path + '.' + data.extension;
+}
+
+fetch('https://gateway.marvel.com:443/v1/public/characters?orderBy=name&limit=20&ts=' + timestamp + '&apikey=' + publicKey +'&hash=' + hash, options)
+.then(function(response) {
+    return response.json();
+})
+.then(function(response) {
+    result = response.data.results;
+    const formSearch = document.querySelector("#form-search");
+
+    formSearch.addEventListener('submit', function(event){
+        event.preventDefault();
+        
+        const inputSearch = document.querySelector("#search-hero").value.toLowerCase();
+
+        if ( inputSearch == "" ){
+            alert("erro");
+            return false;
+        }
+
+        resultSearch = result.filter(data => data.name.toLowerCase().includes(inputSearch));
+
+        if ( resultSearch.length > 0 ){
+            changePage(1);
+            pageNumbers();
+            selectedPage();
+            clickPage();
+            addEventListeners();
+        }
+        else {
+            paginationDiv.style.display = "none";
+            divResult.innerHTML = '<article class="hero-list" style="justify-content: center;"><p>Termo não encontrado!</p></article>';
+        }
+
+        return false;
+    });
+
+    if ( result ){
+        changePage(1);
+        pageNumbers();
+        selectedPage();
+        clickPage();
+        addEventListeners();
+    }
+})
+.catch(function(err) { 
+    return err;
+});
---
+++ assets/js/pagination.js
@@ -0,0 +1,106 @@
+function prevPage() {
+    if ( current_page > 1 ) {
+        current_page--;
+        changePage( current_page );
+    }
+}
+
+function startPage() {
+    current_page = 1;
+    changePage(1);
+}
+
+function nextPage() {
+    if ( current_page < numPages() ) {
+        current_page++;
+        changePage(current_page);
+    } 
+}
+
+function endPage() {
+    current_page = numPages();
+    changePage(numPages());
+}
+
+function clickPage() {
+    document.addEventListener('click', function(e) {
+        if(e.target.nodeName == "A" && e.target.classList.contains("clickPageNumber")) {
+            current_page = e.target.textContent;
+            changePage(current_page);
+        }
+    });
+}
+
+function pageNumbers() {
+    let pageNumber = document.getElementById('page-number');
+        pageNumber.innerHTML = "";
+
+    for(let i = 1; i < numPages() + 1; i++) {
+        pageNumber.innerHTML += "<a class='clickPageNumber'>" + i + "</a>";
+    }
+}
+
+function numPages() {
+    const finalData = resultSearch.length > 0 ? resultSearch : result;
+    return Math.ceil(finalData.length / records_per_page);  
+}
+
+function addEventListeners() {
+    prevButton.addEventListener('click', prevPage);
+    nextButton.addEventListener('click', nextPage);
+    startButton.addEventListener('click', startPage);
+    endButton.addEventListener('click', endPage); 
+}
+      
+function selectedPage() {
+    let page_number = document.getElementById('page-number').getElementsByClassName('clickPageNumber'); 
+    
+    for (let i = 0; i < page_number.length; i++) {
+        if (i == current_page - 1) {
+            page_number[i].classList.add('active');
+        } 
+        else {
+            page_number[i].classList.remove('active');
+        }
+    }   
+}
+
+function changePage(page) {
+    if (page < 1) {
+        page = 1;
+    } 
+    if (page > (numPages() -1)) {
+        page = numPages();
+    }
+ 
+    divResult.innerHTML = "";
+    paginationDiv.style.display = "flex";
+
+    const finalData = resultSearch.length > 0 ? resultSearch : result;
+    console.log(finalData);
+
+    for (let i = (page -1) * records_per_page; i < (page * records_per_page) && i < finalData.length; i++) {
+        divResult.innerHTML += '<article class="hero-list">'+
+                                    '<div class="hero-list-container"><div class="hero">'+
+                                        '<span class="hero-photo"><img src="' + thumbnail(finalData[i].thumbnail) + '" alt="' + finalData[i].name + '"></span>'+
+                                        '<span class="hero-name"><h4>' + finalData[i].name + '</h4></span>'+
+                                    '</div>'+
+                                    '<div class="hero-series">' + returnItems(finalData[i].series.items) + '</div>'+
+                                    '<div class="hero-events">' + returnItems(finalData[i].events.items) + '</div></div>'+
+                                    '<div class="hero-completed">'+
+                                        '<div class="hero-series">'+
+                                            '<h4>Description</h4>'+
+                                            '<p>' + finalData[i].description  + '</>' +
+                                        '</div>'+
+                                        '<div class="hero-series">'+
+                                            '<h4>Stories</h4>'+ returnItems(finalData[i].stories.items) + 
+                                        '</div>'+
+                                        '<div class="hero-series">'+
+                                            '<h4>Comics</h4>'+ returnItems(finalData[i].comics.items) + 
+                                        '</div>'+
+                                    '</div>'+
+                                '</article>';
+    }
+
+    selectedPage();
+}
